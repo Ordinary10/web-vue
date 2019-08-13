6972fb94e1d4a0eb936ca1534bed9413779f6110
luozhiqiang <luozhiqiang@3965682d-3f85-40be-9801-9302a50f8686>
2019-08-13 09:15:07 +0000

    更新全局方法和过滤器

--- src/utils/global_filter.js
+++ src/utils/global_filter.js
@@ -1,7 +1,16 @@
 /* vue全局过滤器 */
 const filters = {
+  // 对返回的年月日时分秒时间取年月日
   dateMonth: function (ctime) {
-    return ctime.split(' ')[0]
+    return ctime ? ctime.split(' ')[0] : ''
+  },
+  // 金额数据处理,大于一万返回xx万，反之返回自身并保留2位小数
+  amount_processing: function (amount) {
+    if (Math.abs(amount) < 10000) {
+      return amount.toFixed(2)
+    } else {
+      return (amount / 10000).toFixed(2) + '万'
+    }
   }
 }
 export default filters
