b9b21ac0be0677b3cb6bc5899010a93184556e95
luozhiqiang <luozhiqiang@3965682d-3f85-40be-9801-9302a50f8686>
2019-08-13 09:15:07 +0000

    更新全局方法和过滤器

--- web-vue/src/utils/common.js
+++ web-vue/src/utils/common.js
@@ -1,6 +1,7 @@
 /* 全局方法对象 */
+// const API_PATH = 'http://test.c.zdxrchina.com/index/index'
+// const login_data = JSON.parse(window.sessionStorage.getItem('loginData'))
 const common = {
-  // 日期格式  获取现在或者几天后的时间字符串格式
   /**
    * day 几天后 number
    * */
@@ -18,6 +19,78 @@ const common = {
       strDate = '0' + strDate
     }
     return year + seperator1 + month + seperator1 + strDate
+  },
+  /*
+  * 金额数据处理
+  * 大于一万返回xx万，反之返回自身并保留2位小数
+  * */
+  amount_processing: function (amount) {
+    if (Math.abs(amount) < 10000) {
+      return amount.toFixed(2)
+    } else {
+      return (amount / 10000).toFixed(2) + '万'
+    }
+  },
+  /*
+* 浮点数运算函数
+* */
+  // 加法
+  float_add: function (arg1, arg2) {
+    if (!(typeof (arg1) === 'number') && (arg1 !== Infinity) && !isNaN(arg1)) {
+      return 0
+    }
+    if (!(typeof (arg2) === 'number') && (arg2 !== Infinity) && !isNaN(arg2)) {
+      return 0
+    }
+    let r1, r2, m
+    try { r1 = arg1.toString().split('.')[1].length } catch (e) { r1 = 0 }
+    try { r2 = arg2.toString().split('.')[1].length } catch (e) { r2 = 0 }
+    m = Math.pow(10, Math.max(r1, r2))
+    return (arg1 * m + arg2 * m) / m
+  },
+  // 减法
+  float_sub: function (arg1, arg2) {
+    if (!(typeof (arg1) === 'number') && (arg1 !== Infinity) && !isNaN(arg1)) {
+      return 0
+    }
+    if (!(typeof (arg2) === 'number') && (arg2 !== Infinity) && !isNaN(arg2)) {
+      return 0
+    }
+    let r1, r2, m, n
+    try { r1 = arg1.toString().split('.')[1].length } catch (e) { r1 = 0 }
+    try { r2 = arg2.toString().split('.')[1].length } catch (e) { r2 = 0 }
+    m = Math.pow(10, Math.max(r1, r2))
+    n = (r1 >= r2) ? r1 : r2
+    return ((arg1 * m - arg2 * m) / m).toFixed(n)
+  },
+  // 除法
+  float_div: function (arg1, arg2) {
+    if (!(typeof (arg1) === 'number') && (arg1 !== Infinity) && !isNaN(arg1)) {
+      return 0
+    }
+    if (!(typeof (arg2) === 'number') && (arg2 !== Infinity) && !isNaN(arg2)) {
+      return 0
+    }
+    let t1, t2, r1, r2
+    try { t1 = arg1.toString().split('.')[1].length } catch (e) { t1 = 0 }
+    try { t2 = arg2.toString().split('.')[1].length } catch (e) { t2 = 0 }
+    r1 = Number(arg1) * Math.pow(10, Math.max(t1, t2))
+    r2 = Number(arg2) * Math.pow(10, Math.max(t1, t2))
+    return (r1 / r2)
+  },
+  // 乘法
+  float_mul: function (arg1, arg2) {
+    if (!(typeof (arg1) === 'number') && (arg1 !== Infinity) && !isNaN(arg1)) {
+      return 0
+    }
+    if (!(typeof (arg2) === 'number') && (arg2 !== Infinity) && !isNaN(arg2)) {
+      return 0
+    }
+    // eslint-disable-next-line one-var
+    let m = 0, s1 = arg1.toString(), s2 = arg2.toString()
+    try { m += s1.split('.')[1].length } catch (e) {}
+    try { m += s2.split('.')[1].length } catch (e) {}
+    return Number(s1.replace('.', '')) * Number(s2.replace('.', '')) / Math.pow(10, m)
   }
 }
 
