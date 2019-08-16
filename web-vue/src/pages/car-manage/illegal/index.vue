1b35e46ad48898de9ff5fdf36a744fbc1528cc77
luozhiqiang <luozhiqiang@3965682d-3f85-40be-9801-9302a50f8686>
2019-08-16 02:24:16 +0000

    搜索模块初步完成

--- web-vue/src/pages/car-manage/illegal/index.vue
+++ web-vue/src/pages/car-manage/illegal/index.vue
@@ -1,6 +1,28 @@
 <template>
     <div>
-      <search></search>
+      <search>
+        <div class="search-box">
+          <Input class="search-input" v-model="searchData.plate_no" size="large" placeholder="请输入车牌" />
+          <Select v-model="searchData.status" class="search-input" size="large" placeholder="请选择状态">
+            <Option v-for="item in statusList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+          </Select>
+          <div class="search-submit">
+            <Button class="search-btn " size="large" icon="md-search" type="primary" @click.native="search"></Button>
+            <Button class="refresh-btn search-btn" size="large" icon="md-refresh" type="info" @click.native="refresh"></Button>
+          </div>
+          <div class="redundant-btn">
+            <Dropdown>
+              <Button type="primary" size="large">
+                更多操作
+                <Icon type="ios-arrow-down"></Icon>
+              </Button>
+              <DropdownMenu slot="list">
+                <DropdownItem @click.native="uploadList">批量导入</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+        </div>
+      </search>
       <div class="content-block">
         <paging-table :config="config"></paging-table>
       </div>
@@ -22,11 +44,36 @@ export default {
           {key: 'query_time', title: '更新日期'},
           {key: 'is_error', title: '查询结果'}
         ]
-      }
+      },
+      searchData: {
+        plate_no: '',
+        status: ''
+      },
+      statusList: [
+        {
+          value: 1,
+          label: '状态1'
+        },
+        {
+          value: 2,
+          label: '状态2'
+        }
+      ]
     }
   },
   mounted () {
 
+  },
+  methods: {
+    uploadList () {
+      alert('批量导入')
+    },
+    search () {
+      alert(`搜索条件：${JSON.stringify(this.searchData)}`)
+    },
+    refresh () {
+      alert('刷新')
+    }
   }
 }
 </script>
