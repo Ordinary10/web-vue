b19e63c894f75ea75347be6125c22d35ecbacb28
zhanghongwei <zhanghongwei@3965682d-3f85-40be-9801-9302a50f8686>
2019-08-21 08:32:05 +0000

    对空的数据进行处理，避免vue进行提示

--- src/components/headPublic/userVehicleHead.vue
+++ src/components/headPublic/userVehicleHead.vue
@@ -34,26 +34,6 @@
             <div class="vehicle-icon">
               <i class="iconfont iconcheliangxinxi fs-28"></i>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="81px" height="82px">
-              <defs>
-                <filter filterUnits="userSpaceOnUse" id="Filter_0" x="0px" y="0px" width="81px" height="82px">
-                  <feOffset in="SourceAlpha" dx="0" dy="4"></feOffset>
-                  <feGaussianBlur result="blurOut" stdDeviation="3"></feGaussianBlur>
-                  <feFlood flood-color="rgb(0, 0, 0)" result="floodOut"></feFlood>
-                  <feComposite operator="atop" in="floodOut" in2="blurOut"></feComposite>
-                  <feComponentTransfer>
-                    <feFuncA type="linear" slope="0.19"></feFuncA>
-                  </feComponentTransfer>
-                  <feMerge>
-                    <feMergeNode></feMergeNode>
-                    <feMergeNode in="SourceGraphic"></feMergeNode>
-                  </feMerge>
-                </filter>
-              </defs>
-              <g filter="url(#Filter_0)">
-                <path fill-rule="evenodd" fill="rgb(18, 105, 219)" d="M5.000,69.987 L5.000,9.000 C5.000,6.791 6.791,5.000 9.000,5.000 L69.000,5.000 C69.000,5.000 69.000,5.000 69.000,5.000 C69.000,40.564 40.437,69.452 5.000,69.987 Z"></path>
-              </g>
-            </svg>
           </div>
           <p class="data-btn vehicle-btn">展<br>开<br>资<br>料<br></p>
         </div>
@@ -93,32 +73,13 @@
             <div class="vehicle-icon">
               <i class="iconfont iconyonghu fs-24"></i>
             </div>
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="81px" height="82px">
-              <defs>
-                <filter filterUnits="userSpaceOnUse" id="Filter_0" x="0px" y="0px" width="81px" height="82px">
-                  <feOffset in="SourceAlpha" dx="0" dy="4"></feOffset>
-                  <feGaussianBlur result="blurOut" stdDeviation="3"></feGaussianBlur>
-                  <feFlood flood-color="rgb(0, 0, 0)" result="floodOut"></feFlood>
-                  <feComposite operator="atop" in="floodOut" in2="blurOut"></feComposite>
-                  <feComponentTransfer>
-                    <feFuncA type="linear" slope="0.19"></feFuncA>
-                  </feComponentTransfer>
-                  <feMerge>
-                    <feMergeNode></feMergeNode>
-                    <feMergeNode in="SourceGraphic"></feMergeNode>
-                  </feMerge>
-                </filter>
-              </defs>
-              <g filter="url(#Filter_0)">
-                <path fill-rule="evenodd" fill="rgb(252, 97, 128)" d="M5.000,69.987 L5.000,9.000 C5.000,6.791 6.791,5.000 9.000,5.000 L69.000,5.000 C69.000,5.000 69.000,5.000 69.000,5.000 C69.000,40.564 40.437,69.452 5.000,69.987 Z"></path>
-              </g>
-            </svg>
           </div>
           <p class="data-btn vehicle-btn">展<br>开<br>资<br>料<br></p>
         </div>
       </div>
     </Col>
   </Row>
+
 
 </template>
 
@@ -135,7 +96,6 @@
       }
     },
     created(){
-      console.log(123)
       this.getlinst()
     },
     computed:{
@@ -169,11 +129,16 @@
        async getlinst() {
          const _this = this
          var res  = await _this.$axios('Common/getCommonalityHead',{plate_no:this.param})
+         for (let k in res.data){
+           console.log(k,res.data[k])
+           if (!res.data[k]){
+             res.data[k]='暂无'
+           }
+         }
          this.addData  = res.data
       },
 
       clean(list,data){
-        var newarray = []
         for (let key in list){
           for (let k in data){
             if (k ==list[key]) {
