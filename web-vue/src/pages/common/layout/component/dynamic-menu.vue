ae1b6c013e732c7b23688b5236e2211cae4ba7bc
xieyu <xieyu@3965682d-3f85-40be-9801-9302a50f8686>
2019-08-21 09:52:19 +0000

    tab 点击div增大

--- web-vue/src/pages/common/layout/component/dynamic-menu.vue
+++ web-vue/src/pages/common/layout/component/dynamic-menu.vue
@@ -4,7 +4,7 @@
     <template v-for="v in menuList">
       <Submenu v-if="v.children&&v.children.length>0" :key="v.name" :name="v.name" >
         <template slot="title">
-          <div ref="title" :data-name="v.name" :data-title="v.meta.tabName">
+          <div ref="title" :data-name="v.name" :data-title="v.meta.tabName" style="width: 100%">
             <Icon :custom="'iconfont ' + v.meta.icon" v-if="v.meta.icon"></Icon>
             {{v.meta.name}}
           </div>
